570b027cec2421e92f00b86153eb7b1c0b03f0b0
JiMPulse <jimpulse@anargeek.net>
2014-12-25 19:03:54 +0100

    xilinx/zynq : No I2C eeprom mux on redpitaya

--- board/xilinx/zynq/board.c
+++ board/xilinx/zynq/board.c
@@ -32,7 +32,7 @@ static xilinx_desc fpga100 = XILINX_XC7Z100_DESC(0x100);
 
 int board_init(void)
 {
-#if defined(CONFIG_ENV_IS_IN_EEPROM) && !defined(CONFIG_SPL_BUILD)
+#if defined(CONFIG_ENV_IS_IN_EEPROM) && !defined(CONFIG_SPL_BUILD) && defined(CONFIG_SYS_I2C_MUX_EEPROM_SEL)
 	unsigned char eepromsel = CONFIG_SYS_I2C_MUX_EEPROM_SEL;
 #endif
 #if (defined(CONFIG_FPGA) && !defined(CONFIG_SPL_BUILD)) || \
@@ -77,7 +77,7 @@ int board_init(void)
 	fpga_init();
 	fpga_add(fpga_xilinx, &fpga);
 #endif
-#if defined(CONFIG_ENV_IS_IN_EEPROM) && !defined(CONFIG_SPL_BUILD)
+#if defined(CONFIG_ENV_IS_IN_EEPROM) && !defined(CONFIG_SPL_BUILD) && defined(CONFIG_SYS_I2C_MUX_EEPROM_SEL)
 	if (eeprom_write(CONFIG_SYS_I2C_MUX_ADDR, 0, &eepromsel, 1))
 		puts("I2C:EEPROM selection failed\n");
 #endif
